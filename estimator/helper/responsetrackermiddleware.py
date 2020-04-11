23f17bff89f9da61fa5bfb3e76796aab8e936129
Mustapha Omotunwase <mustapha.omotunwase@ubagroup.com>
2020-04-11 21:55:54 +0100

    covid-19-API-completed

---
+++ estimator/helper/responsetrackermiddleware.py
@@ -0,0 +1,17 @@
+import time
+from estimator.helper.file import writeToFile
+
+class StatsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time.time()
+
+        response = self.get_response(request)
+
+        duration = time.time() - start_time
+        timeTaken = int(round(duration * 1000))
+        print(timeTaken)
+        writeToFile(request.method, request.path, response.status_code, timeTaken)
+        return response
