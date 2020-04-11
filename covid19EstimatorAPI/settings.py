7530bb497dc98796cc9e312edfa243f011481912
Mustapha Omtunwase <35955844+moostee@users.noreply.github.com>
2020-04-11 22:16:02 +0100

    removed cors header from settings.py

--- covid19EstimatorAPI/settings.py
+++ covid19EstimatorAPI/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,9 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'estimator.helper.responsetrackermiddleware.StatsMiddleware'
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_HEADERS = []
 
 ROOT_URLCONF = 'covid19EstimatorAPI.urls'
 
