2374f610e391674bd15dc04dbf26f37b6c234ec7
Mustapha Omtunwase <35955844+moostee@users.noreply.github.com>
2020-04-11 22:36:46 +0100

    removed static file

--- covid19EstimatorAPI/settings.py
+++ covid19EstimatorAPI/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
